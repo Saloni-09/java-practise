33445998b33635a27160430f523a46029ccfc068
saloni <saloni080103@gmail.com>
2024-08-05 22:12:13 +0530

    second commit

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class sample {
     public static void main(String args[]) {
-        System.out.println("hello world");
+        System.out.println("hello world, my name is saloni");
     }
 }
